8f3f0cf45d7c6740f711bf70ea47ce8e02206b78
huanghaijie <huanghaijie@58ganji.com>
2018-05-25 19:28:11 +0800

    注册

--- app/src/main/java/com/hankkin/reading/http/HttpFactory.kt
+++ app/src/main/java/com/hankkin/reading/http/HttpFactory.kt
@@ -1,7 +1,20 @@
 package com.hankkin.reading.http
 
+import java.lang.ref.WeakReference
+
 /**
  * Created by huanghaijie on 2018/5/23.
  */
-class HttpFactory{
+class HttpFactory {
+
+    val map = HashMap<Class<*>, WeakReference<Any>>()
+
+    fun <T> getService(clazz: Class<T>): WeakReference<Any>? {
+        if (!map.containsKey(clazz)) {
+            val value = HttpClient.getnorRetrofit().create(clazz)
+            val realValue = WeakReference(value)
+            map.put(clazz, realValue)
+        }
+        return map[clazz]
+    }
 }
