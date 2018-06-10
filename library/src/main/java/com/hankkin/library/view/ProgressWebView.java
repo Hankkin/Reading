b25b02f05feb5ffa80a89731fff429386011ff0a
Hankkin <1019283569@qq.com>
2018-06-10 23:21:53 +0800

    授权登录

---
+++ library/src/main/java/com/hankkin/library/view/ProgressWebView.java
@@ -0,0 +1,69 @@
+package com.hankkin.library.widget.view;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+
+import com.hankkin.library.R;
+
+
+/**
+ * Created by hankkin on 2017/5/11.
+ */
+
+public class ProgressWebView extends WebView {
+    private static ProgressBar mProgressBar;
+
+    public ProgressWebView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        mProgressBar = new ProgressBar(context, null,
+                android.R.attr.progressBarStyleHorizontal);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT, 8);
+        mProgressBar.setLayoutParams(layoutParams);
+
+        Drawable drawable = context.getResources().getDrawable(
+                R.drawable.web_progress_bar_states);
+        mProgressBar.setProgressDrawable(drawable);
+        addView(mProgressBar);
+        setWebChromeClient(new WebChromeClient());
+        setLayerType(View.LAYER_TYPE_SOFTWARE, null);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        invalidate();
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    public static class WebChromeClient extends android.webkit.WebChromeClient {
+        @Override
+        public void onProgressChanged(WebView view, int newProgress) {
+            if (newProgress == 100) {
+                mProgressBar.setVisibility(GONE);
+            } else {
+                if (mProgressBar.getVisibility() == GONE)
+                    mProgressBar.setVisibility(VISIBLE);
+                mProgressBar.setProgress(newProgress);
+            }
+            super.onProgressChanged(view, newProgress);
+        }
+    }
+
+    @Override
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
+        LayoutParams lp = (LayoutParams) mProgressBar.getLayoutParams();
+        lp.x = l;
+        lp.y = t;
+        mProgressBar.setLayoutParams(lp);
+        super.onScrollChanged(l, t, oldl, oldt);
+    }
+
+    public void hideProgress() {
+        mProgressBar.setVisibility(GONE);
+    }
+}
