d9cefc8a11f436bb0b09d7e3e7a2111ed1108087
Hankkin <1019283569@qq.com>
2018-08-01 01:03:41 +0800

    ``

--- app/src/main/java/com/hankkin/reading/ui/tools/KuaiDiActivity.kt
+++ app/src/main/java/com/hankkin/reading/ui/tools/KuaiDiActivity.kt
@@ -1,8 +1,10 @@
 package com.hankkin.reading.ui.tools
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
 import com.afollestad.materialdialogs.MaterialDialog
 import com.hankkin.reading.R
 import com.hankkin.reading.base.BaseActivity
@@ -11,9 +13,13 @@ import com.hankkin.reading.ui.home.articledetail.CommonWebActivity
 import com.hankkin.reading.utils.ViewHelper
 import kotlinx.android.synthetic.main.activity_kuai_di.*
 import kotlinx.android.synthetic.main.layout_title_bar_back.*
+import android.content.Context.INPUT_METHOD_SERVICE
+import android.text.InputType
+import android.view.inputmethod.InputMethodManager
+
 
 class KuaiDiActivity : BaseActivity() {
-     var KUAIDI_URL = "https://m.kuaidi100.com/index_all.html"
+    var KUAIDI_URL = "https://m.kuaidi100.com/index_all.html"
     private lateinit var mCompanys: MutableList<KuaiDiCompanyBean>
     private lateinit var mSelectCompany: KuaiDiCompanyBean
 
@@ -26,29 +32,39 @@ class KuaiDiActivity : BaseActivity() {
 
     override fun initViews(savedInstanceState: Bundle?) {
         mCompanys = mutableListOf()
-        mCompanys.add(KuaiDiCompanyBean("debangwuliu","德邦物流"))
-        mCompanys.add(KuaiDiCompanyBean("shentong","申通快递"))
-        mCompanys.add(KuaiDiCompanyBean("shunfeng","顺丰快递"))
-        mCompanys.add(KuaiDiCompanyBean("tiantian","天天快递"))
-        mCompanys.add(KuaiDiCompanyBean("yuantong","圆通速递"))
-        mCompanys.add(KuaiDiCompanyBean("yunda","韵达快运"))
+        mCompanys.add(KuaiDiCompanyBean("debangwuliu", "德邦物流"))
+        mCompanys.add(KuaiDiCompanyBean("shentong", "申通快递"))
+        mCompanys.add(KuaiDiCompanyBean("shunfeng", "顺丰快递"))
+        mCompanys.add(KuaiDiCompanyBean("tiantian", "天天快递"))
+        mCompanys.add(KuaiDiCompanyBean("yuantong", "圆通速递"))
+        mCompanys.add(KuaiDiCompanyBean("yunda", "韵达快运"))
         var data = mutableListOf<String>()
-        for (c in mCompanys){
+        for (c in mCompanys) {
             data.add(c.name)
         }
         tv_normal_title.text = resources.getString(R.string.tools_kuaidi_title)
         iv_back_icon.setOnClickListener { finish() }
         et_kuaidi_number.addTextChangedListener(watcher)
         et_kuaidi_company.addTextChangedListener(watcher)
-        et_kuaidi_company.setOnClickListener { ViewHelper.showListNoTitleDialog(this,data,
+        et_kuaidi_company.onFocusChangeListener = View.OnFocusChangeListener { v, hasFocus ->
+            if(hasFocus){
+                showCompany(data)
+            }
+        }
+        et_kuaidi_company.setOnClickListener { showCompany(data) }
+        btn_kuaidi.setOnClickListener {
+            CommonWebActivity.loadUrl(this, KUAIDI_URL + "?type=" + mSelectCompany.code + "&postid=" + et_kuaidi_number.text.toString(), "查询结果")
+        }
+    }
+
+    private fun showCompany(data: MutableList<String>){
+        et_kuaidi_company.inputType = InputType.TYPE_NULL
+        ViewHelper.showListNoTitleDialog(this, data,
                 MaterialDialog.ListCallback { dialog, itemView, which, text ->
-                    et_kuaidi_company.text = mCompanys[which].name
+                    et_kuaidi_company.setText(mCompanys[which].name)
                     mSelectCompany = mCompanys[which]
                     dialog.dismiss()
-                }) }
-        btn_kuaidi.setOnClickListener {
-            CommonWebActivity.loadUrl(this,KUAIDI_URL+"?type="+mSelectCompany.code+"&postid="+et_kuaidi_number.text.toString(),"查询结果")
-        }
+                })
     }
 
 
