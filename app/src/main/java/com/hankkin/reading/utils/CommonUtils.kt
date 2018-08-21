c484845a767a81175bc3e1c10887a3eb15f876bd
huanghaijie <huanghaijie@58ganji.com>
2018-08-21 16:58:30 +0800

    反馈

--- app/src/main/java/com/hankkin/reading/utils/CommonUtils.kt
+++ app/src/main/java/com/hankkin/reading/utils/CommonUtils.kt
@@ -22,4 +22,13 @@ object CommonUtils {
         intent.setData(Uri.parse(url))
         context.startActivity(intent)
     }
+
+    fun feedBack(context: Activity){
+        val intent = Intent(Intent.ACTION_SEND)
+        intent.type = "message/rfc822"
+        intent.putExtra(Intent.EXTRA_EMAIL,"1019283569@qq.com")
+        intent.putExtra(Intent.EXTRA_SUBJECT,"您的建议")
+        intent.putExtra(Intent.EXTRA_TEXT,"希望能得到您的建议")
+        context.startActivity(Intent.createChooser(intent,"Select email application"))
+    }
 }
