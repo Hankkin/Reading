3bfc4b643365e244611384fcdf676838d0f54c82
huanghaijie <huanghaijie@58ganji.com>
2018-08-22 19:54:15 +0800

    webview支持下载

--- app/src/main/java/com/hankkin/reading/ui/home/articledetail/CommonWebActivity.kt
+++ app/src/main/java/com/hankkin/reading/ui/home/articledetail/CommonWebActivity.kt
@@ -3,6 +3,7 @@ package com.hankkin.reading.ui.home.articledetail
 import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Build
 import android.view.Menu
 import android.view.MenuItem
@@ -72,41 +73,48 @@ class CommonWebActivity : BaseMvpActivity<ArticleDetailPresenter>(), ArticleDeta
     }
 
     private fun initWebView() {
-        ws = web_article.getSettings()
+        ws = web_article.settings
         // 网页内容的宽度是否可大于WebView控件的宽度
-        ws.setLoadWithOverviewMode(false)
+        ws.loadWithOverviewMode = false
         // 保存表单数据
-        ws.setSaveFormData(true)
+        ws.saveFormData = true
         // 是否应该支持使用其屏幕缩放控件和手势缩放
         ws.setSupportZoom(true)
-        ws.setBuiltInZoomControls(true)
-        ws.setDisplayZoomControls(false)
+        ws.builtInZoomControls = true
+        ws.displayZoomControls = false
         // 启动应用缓存
         ws.setAppCacheEnabled(true)
         // 设置缓存模式
-        ws.setCacheMode(WebSettings.LOAD_DEFAULT)
+        ws.cacheMode = WebSettings.LOAD_DEFAULT
         // setDefaultZoom  api19被弃用
         // 设置此属性，可任意比例缩放。
-        ws.setUseWideViewPort(true)
+        ws.useWideViewPort = true
         // 不缩放
         web_article.setInitialScale(100)
         // 告诉WebView启用JavaScript执行。默认的是false。
-        ws.setJavaScriptEnabled(true)
+        ws.javaScriptEnabled = true
         //  页面加载好以后，再放开图片
-        ws.setBlockNetworkImage(false)
+        ws.blockNetworkImage = false
         // 使用localStorage则必须打开
-        ws.setDomStorageEnabled(true)
+        ws.domStorageEnabled = true
         // 排版适应屏幕
-        ws.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS)
+        ws.layoutAlgorithm = WebSettings.LayoutAlgorithm.NARROW_COLUMNS
         // WebView是否新窗口打开(加了后可能打不开网页)
         //        ws.setSupportMultipleWindows(true);
 
         // webview从5.0开始默认不允许混合模式,https中不能加载http资源,需要设置开启。
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ws.setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW)
+            ws.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
         }
         /** 设置字体默认缩放大小(改变网页字体大小,setTextSize  api14被弃用) */
-        ws.setTextZoom(100)
+        ws.textZoom = 100
+
+        web_article.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.addCategory(Intent.CATEGORY_BROWSABLE)
+            intent.data = Uri.parse(url)
+            startActivity(intent)
+        }
     }
 
 
